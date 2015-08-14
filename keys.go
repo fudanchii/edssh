aa364b427e68608262ef5368c8f2e38055424a32
Nurahmadie <nurahmadie@gmail.com>
2015-08-14 11:30:33 +0700

    Implement ssh.Signer and ssh.PublicKey

--- keys.go
+++ keys.go
@@ -1,6 +1,7 @@
 package edssh
 
 import (
+	"encoding/base64"
 	"encoding/pem"
 	"github.com/agl/ed25519"
 	"golang.org/x/crypto/ssh"
@@ -18,21 +19,14 @@ func ParsePrivateKey(pemBytes []byte) (ssh.Signer, error) {
 		if err != nil {
 			return nil, err
 		}
-		return NewSignerFromEd25519Key(key)
+		return key, nil
 	default:
 		return ssh.ParsePrivateKey(pemBytes)
 	}
 }
 
-func NewSignerFromEd25519Key(prv *Ed25519PrivateKey) (ssh.Signer, error) {
-}
-
 type Ed25519PrivateKey struct {
 	bytes *[ed25519.PrivateKeySize]byte
-}
-
-type Ed25519PublicKey struct {
-	bytes *[ed25519.PublicKeySize]byte
 }
 
 func ParseEd25519PrivateKey(keyBlock []byte) (*Ed25519PrivateKey, error) {
@@ -97,14 +91,48 @@ func (ek *Ed25519PrivateKey) parseFromBuf(buf []byte, prevErr error) error {
 		return errors.New("edssh: invalid private key length")
 	}
 
-	*ek.bytes = privKey
+	pk := make([]byte, ed25519.PrivateKeySize)
+	copy(pk, privKey)
+	ek.bytes = &pk
 
 	return nil
 }
 
 func (ek *Ed25519PrivateKey) Public() ssh.PublicKey {
-	return &Ed25519PublicKey{ek.public}
+	pub = make([]byte, ed25519.PublicKeySize)
+	copy(pub, ek.bytes[32:])
+	return &Ed25519PublicKey{&pub}
 }
 
 func (ek *Ed25519PrivateKey) Sign(rand io.Reader, data []byte) (*ssh.Signature, error) {
+	signature := ed25519.Sign(ek.bytes, data)
+	return &ssh.Signature{
+		Format: ek.Public().Type(),
+		Blob:   *signature,
+	}, nil
+}
+
+type Ed25519PublicKey struct {
+	bytes *[ed25519.PublicKeySize]byte
+}
+
+func (ek *Ed25519PublicKey) Type() string {
+	return "ssh-ed25519"
+}
+
+func (ek *Ed25519PublicKey) Marshal() []byte {
+	var buf []byte
+	buf = append(buf, []byte(ek.Type())...)
+	buf = append(buf, 0x20, []byte(base64.StdEncoding.EncodeToString(ek.bytes))...)
+	return buf
+}
+
+func (ek *Ed25519PublicKey) Verify(message []byte, signature *ssh.Signature) error {
+	if signature.Format != ek.Type() {
+		return errors.New("edssh: signature type %s for key type %s", signature.Format, ek.Type())
+	}
+	if ok := ed25519.Verify(ek.bytes, message, signature.Blob); !ok {
+		return errors.New("edssh: invalid signature for given message")
+	}
+	return nil
 }
